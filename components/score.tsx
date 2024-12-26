a19e6c41b5a98768171e8dfe9dd06cdd7f19f8e9
essenlive <perchais.quentin@gmail.com>
2024-12-26 11:47:55 +0100

    Added grid size

--- components/score.tsx
+++ components/score.tsx
@@ -1,19 +1,23 @@
 "use client"
 
 import React from 'react';
-import { Star } from 'lucide-react'
+import { Square, Star } from 'lucide-react'
 
 import { useSelector } from '@xstate/react';
 import { swapActor } from "@/lib/swapMachine"
 
 const selectScore = ({ context }) => context.score
+const selectCanvaSize = ({ context }) => context.canvaSize
 
 const Score: React.FC = () => {
     const score = useSelector(swapActor, selectScore);
+    const canvaSize = useSelector(swapActor, selectCanvaSize);
     return (
         <div className="flex items-center">
             <Star className="w-5 h-5 mr-2" />
-            <span className="text-lg md:text-xl font-semibold">{score}</span>
+            <span className="text-lg md:text-xl font-semibold mr-5">{score}</span>
+            <Square className="w-5 h-5 mr-2" />
+            <span className="text-lg md:text-xl font-semibold">{canvaSize.height * canvaSize.width}</span>
         </div>
     );
 };
