c571f6ff564bf856c9886e6a325129aeea485d77
essenlive <perchais.quentin@gmail.com>
2025-02-14 19:01:19 +0100

    Added no inputs info

--- components/inputList.tsx
+++ components/inputList.tsx
@@ -34,31 +34,39 @@ export function InputList({
     const scrollToRef = useRef<HTMLDivElement>(null);
 
     return (
-        
-        <ScrollArea className="w-full whitespace-nowrap rounded-md border">
-            <div className={cn("flex space-x-4 items-center min-h-6 my-4", className)}>
-                <AnimatePresence>
-                    {inputList.map((input, i) => (
-
-                        <motion.div
-                            ref={i === inputIndex ? scrollToRef : null}
-                            whileHover={{ scale: 1.05 }}
-                            key={`input-${i}`}
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            exit={{ opacity: 0 }}
-                            transition={transition}
-                            className={cn(
-                                i === inputIndex && "bg-background ring-1 ring-inset",
-                                "cursor-pointer aspect-square w-6 h-6 rounded-sm flex justify-center items-center align-middle text-center"
-                            )}
-                        >
-                            {DIRECTION_ARROWS.get(input)}
-                        </motion.div>
-                    ))}
-                </AnimatePresence>
+       <>
+        { inputList.length === 0 ? (
+            <div className='min-h-6 my-4 font-semibold'>
+                No inputs logged yet. Use swipes or arrows to move.
             </div>
-        <ScrollBar orientation="horizontal" />
-    </ScrollArea>
+        ) : (
+            <ScrollArea className="w-full whitespace-nowrap rounded-md border">
+                <div className={cn("flex space-x-2 items-center min-h-6 my-4", className)}>
+                    <AnimatePresence>
+                        {inputList.map((input, i) => (
+
+                            <motion.div
+                                ref={i === inputIndex ? scrollToRef : null}
+                                whileHover={{ scale: 1.05 }}
+                                key={`input-${i}`}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={transition}
+                                className={cn(
+                                    i === inputIndex && "bg-background ring-1 ring-inset",
+                                    "cursor-pointer aspect-square w-6 h-6 rounded-sm flex justify-center items-center align-middle text-center"
+                                )}
+                            >
+                                {DIRECTION_ARROWS.get(input)}
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
+                </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+        ) }
+
+       </> 
     )
 }
